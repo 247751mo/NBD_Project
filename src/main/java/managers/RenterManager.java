4aeb63ce56b9e497f48a35167b7d270ca54d5644
247751mo <247751@edu.p.lodz.pl>
2024-10-20 12:59:58 +0200

    kolejny update z przepisywania kodu z POBI#2

--- src/main/java/managers/RenterManager.java
+++ src/main/java/managers/RenterManager.java
@@ -1,4 +1,26 @@
-package managers;
+
+import java.util.List;
+import model.Renter;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
 
 public class RenterManager {
+
+    private EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("my-persistence-unit");
+
+    public void saveRenter(Renter renter) {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        entityManager.persist(renter);
+        entityManager.getTransaction().commit();
+        entityManager.close();
+    }
+
+    public List<Renter> getAllRenters() {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        List<Renter> renters = entityManager.createQuery("SELECT r FROM Renter r", Renter.class).getResultList();
+        entityManager.close();
+        return renters;
+    }
 }
