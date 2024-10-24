3fe3d071bd5e2a8a4b818db42cb0283380e4560d
247751mo <247751@edu.p.lodz.pl>
2024-10-24 18:58:37 +0200

    RentRepoTest dalej

--- src/test/java/RentRepoTest.java
+++ src/test/java/RentRepoTest.java
@@ -45,5 +45,42 @@ class RentRepoTest {
 
         assertEquals(rent, foundRent);
     }
+    @Test
+    void testRemoveRent() {
+        Renter renter = new Renter("Tyler", "Okonma", "TYLER1", new NoCard());
+        Book book = new Book("Homer", "Odyseja", "Epos");
+
+        renterRepo.add(renter);
+        volumeRepo.add(book);
+
+        Rent rent = new Rent(renter, book, LocalDateTime.now());
+        rentRepo.add(rent);
+
+        Rent foundRent1 = rentRepo.get(rent.getId());
+        assertNotNull(foundRent1);
+
+        rentRepo.delete(rent);
+
+        Rent foundRent2 = rentRepo.get(rent.getId());
+        assertNull(foundRent2);
+    }
+    @Test
+    void testUpdateRent() {
+        Renter renter = new Renter("Kali", "Uchis", "KALI343", new NoCard());
+        Book book = new Book("Boleslaw Prus", "Kamizelka", "Opowiadanie");
+        Rent rent = new Rent(renter, book, LocalDateTime.now());
+
+        em.getTransaction().begin();
+        em.persist(renter);
+        em.persist(book);
+        em.getTransaction().commit();
+
+        rentRepo.add(rent);
+        UUID rentId = rent.getId();
+        assertNotNull(rentId,"The rent ID should not be null after persisting");
+        Rent retrievedRent = rentRepo.get(rentId);
+        assertNotNull(retrievedRent, "The retrieved rent should not be null");
+        assertEquals(rentId, retrievedRent.getId(), "The retrieved rent ID should match the persisted rent ID");
+    }
 
 }
