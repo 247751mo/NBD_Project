48a67bb8b0e35c0a5e60974978f995e1f3bacd74
247778 <247778@edu.p.lodz.pl>
2024-11-20 19:27:35 +0100

    testy volume

--- src/test/java/VolumeRepoTest.java
+++ src/test/java/VolumeRepoTest.java
@@ -1,86 +1,100 @@
-//import jakarta.persistence.EntityManager;
-//import jakarta.persistence.EntityManagerFactory;
-//import jakarta.persistence.Persistence;
-//import model.*;
-//import org.junit.jupiter.api.BeforeAll;
-//import org.junit.jupiter.api.Test;
-//import repositories.VolumeRepo;
-//
-//import java.util.List;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//class VolumeRepoTest {
-//
-//    private static VolumeRepo volumeRepo;
-//
-//    @BeforeAll
-//    public static void setUp() {
-//        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("default");
-//        EntityManager entityManager = entityManagerFactory.createEntityManager();
-//        volumeRepo = new VolumeRepo(entityManager);
-//    }
-//    @Test
-//    void testAddVolume() {
-//        Book book = new Book("Henryk Sienkiewicz", "Potop", "Historyczne");
-//        Monthly monthly = new Monthly("Top Gear", "Motoryzacyjne", "Immediate Media Company");
-//
-//        volumeRepo.create(book);
-//        volumeRepo.create(monthly);
-//
-//        Book foundVolume1 = (Book) volumeRepo.read(book.getVolumeId());
-//        Monthly foundVolume2 = (Monthly) volumeRepo.read(monthly.getVolumeId());
-//
-//        assertEquals(book, foundVolume1);
-//        assertEquals(monthly, foundVolume2);
-//    }
-//    @Test
-//    void testDeleteVolume() {
-//        Book book = new Book("Adam Mickiewicz", "Pan Tadeusz", "Epika");
-//        volumeRepo.create(book);
-//
-//        Book foundBook1 = (Book) volumeRepo.read(book.getVolumeId());
-//        assertNotNull(foundBook1);
-//
-//        volumeRepo.delete(book);
-//
-//        Book foundBook2 = (Book) volumeRepo.read(book.getVolumeId());
-//        assertNull(foundBook2);
-//    }
-//    @Test
-//    void testUpdateVolume() {
-//        Weekly weekly = new Weekly("Fakt", "Wiadomosci", "J. Billig");
-//        volumeRepo.create(weekly);
-//
-//        Weekly foundWeekly1 = (Weekly) volumeRepo.read(weekly.getVolumeId());
-//        assertEquals("Fakt", foundWeekly1.getTitle());
-//        assertEquals("Wiadomosci", foundWeekly1.getGenre());
-//
-//        weekly.setTitle("Tygodnik Sportowy");
-//        weekly.setGenre("Sportowe");
-//
-//        volumeRepo.update(weekly);
-//
-//        Weekly foundWeekly2 = (Weekly) volumeRepo.read(weekly.getVolumeId());
-//        assertEquals("Tygodnik Sportowy", foundWeekly2.getTitle());
-//        assertEquals("Sportowe", foundWeekly2.getGenre());
-//    }
-//    @Test
-//    void testGetAllVolumes() {
-//        List<Volume> volumes = volumeRepo.readAll();
-//        int initialSize = volumes.size();
-//
-//        Book book1 = new Book("Boleslaw Prus", "Lalka", "Powiesc");
-//        Book book2 = new Book("Sofokles", "Krol Edyp", "Tragedia");
-//
-//        volumeRepo.create(book1);
-//        volumeRepo.create(book2);
-//
-//        volumes = volumeRepo.readAll();
-//        int finalSize = volumes.size();
-//
-//        assertEquals(initialSize + 2, finalSize);
-//    }
-//
-//
-//}
+import model.Book;
+import model.Monthly;
+import model.Volume;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import repositories.VolumeRepo;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VolumeRepoTest {
+
+    private static VolumeRepo volumeRepo;
+
+    @BeforeAll
+    public static void setUp() {
+        volumeRepo = new VolumeRepo();
+    }
+
+    @BeforeEach
+    public void cleanUp() {
+        volumeRepo.getDatabase().getCollection("volumes", Volume.class).drop();
+    }
+
+    @AfterAll
+    public static void tearDown() {
+        volumeRepo.getDatabase().getCollection("volumes", Volume.class).drop();
+        volumeRepo.close();
+    }
+
+    @Test
+    void testAddVolume() {
+        Book book1 = new Book("Solaris","Scifi","Stanislaw Lem");
+        Monthly monthly1 = new Monthly("Miesiecznik","Gatunek","Wydawca");
+
+        volumeRepo.create(book1);
+        volumeRepo.create(monthly1);
+
+        Volume foundVolume1 = volumeRepo.read(book1.getVolumeId());
+        Volume foundVolume2 = volumeRepo.read(monthly1.getVolumeId());
+
+        assertEquals(book1, foundVolume1);
+        assertEquals(monthly1, foundVolume2);
+    }
+
+    @Test
+    void testRemoveVolume() {
+        Book book1 = new Book("Solaris","Scifi","Stanislaw Lem");
+        volumeRepo.create(book1);
+
+        Volume foundVolume = volumeRepo.read(book1.getVolumeId());
+        assertNotNull(foundVolume);
+
+        volumeRepo.delete(book1);
+
+        Volume deletedVolume = volumeRepo.read(book1.getVolumeId());
+        assertNull(deletedVolume);
+    }
+
+    @Test
+    void testUpdateVolume() {
+        Book book1 = new Book("Solaris","Scifi","Stanislaw Lem");
+        volumeRepo.create(book1);
+
+        Volume foundVolumeBeforeUpdate = volumeRepo.read(book1.getVolumeId());
+        assertEquals("Solaris", foundVolumeBeforeUpdate.getTitle());
+        assertEquals("Scifi", foundVolumeBeforeUpdate.getGenre());
+
+        book1.setTitle("NowyTytul");
+        book1.setGenre("ificS");
+        book1.setRented(true);
+
+        volumeRepo.update(book1);
+
+        Volume foundVolumeAfterUpdate = volumeRepo.read(book1.getVolumeId());
+        assertEquals("NowyTytul", foundVolumeAfterUpdate.getTitle());
+        assertEquals("ificS", foundVolumeAfterUpdate.getGenre());
+        assertTrue(foundVolumeAfterUpdate.isRented());
+    }
+
+    @Test
+    void testGetAllVolumes() {
+        List<Volume> volumes = volumeRepo.readAll();
+        int initialSize = volumes.size();
+
+        Book book1 = new Book("Solaris","Scifi","Stanislaw Lem");
+        Monthly monthly1 = new Monthly("Miesiecznik","Gatunek","Wydawca");
+
+        volumeRepo.create(book1);
+        volumeRepo.create(monthly1);
+
+        volumes = volumeRepo.readAll();
+        int finalSize = volumes.size();
+
+        assertEquals(initialSize + 2, finalSize);
+    }
+}
