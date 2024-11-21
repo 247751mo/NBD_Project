3d9a804f600fef116933087cb031dcb41f20a06b
247778 <247778@edu.p.lodz.pl>
2024-11-21 20:13:45 +0100

    1241235

--- src/test/java/RentManagerTest.java
+++ src/test/java/RentManagerTest.java
@@ -5,8 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.MongoClients;
+
 import managers.RentManager;
 import model.Rent;
 import model.Renter;
@@ -17,7 +16,7 @@ import repositories.VolumeRepo;
 
 import java.time.LocalDateTime;
 
-class RentManagerTest {
+public class RentManagerTest {
     private static RentRepo rentRepo;
     private static RenterRepo renterRepo;
     private static VolumeRepo volumeRepo;
@@ -28,7 +27,7 @@ class RentManagerTest {
         rentRepo = new RentRepo();
         renterRepo = new RenterRepo();
         volumeRepo = new VolumeRepo();
-
+        rentManager = new RentManager(rentRepo);
     }
     @BeforeEach
     public void cleanUp() {
@@ -40,26 +39,27 @@ class RentManagerTest {
     @AfterAll
     public static void tearDown() {
         rentRepo.getDatabase().getCollection("rents", Rent.class).drop();
-        rentRepo.getDatabase().getCollection("volumes", Volume.class).drop();
-        rentRepo.getDatabase().getCollection("renters", Renter.class).drop();
+        volumeRepo.getDatabase().getCollection("volumes", Volume.class).drop();
+        renterRepo.getDatabase().getCollection("renters", Renter.class).drop();
         rentRepo.close();
+        renterRepo.close();
+        volumeRepo.close();
     }
     @Test
     void cantRentOneBookTwoTimes(){
         Book volume = new Book(2,"vol2", "Another Volume", "Fantasy");
-        Renter renter1 = new Renter("124", "Doe", "Jane");
-        Renter renter2 = new Renter("123", "Doe", "John");
-        Rent rent1 = new Rent("1", renter1, volume, LocalDateTime.now());
-        Rent rent2 = new Rent("2", renter2, volume, LocalDateTime.now());
+        Renter renter = new Renter("124", "Doe", "Jane");
+
+
         volumeRepo.create(volume);
-        renterRepo.create(renter1);
-        renterRepo.create(renter2);
+        renterRepo.create(renter);
+        volume.setIsRented(1);
 
-        // Test wypożyczenia dwóch książek
-        rentRepo.create(rent1);
-        rentRepo.create(rent2);
+        Exception exception = assertThrows(Exception.class, () -> rentManager.rentVolume(renter,volume,LocalDateTime.now()));
 
+        String expected= "Volume is already rented: " + volume.getTitle();
+        String actual = exception.getMessage();
 
-
+        assertTrue(actual.contains(expected));
     }
 }
