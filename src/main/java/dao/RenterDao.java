730b890b525bea52ffacf82ee6acd5cc1de0ed5a
247778 <247778@edu.p.lodz.pl>
2025-01-15 18:00:21 +0100

    kasandra

--- src/main/java/dao/RenterDao.java
+++ src/main/java/dao/RenterDao.java
@@ -1,4 +1,27 @@
 package dao;
 
+
+import com.datastax.oss.driver.api.mapper.annotations.Dao;
+import com.datastax.oss.driver.api.mapper.annotations.QueryProvider;
+import com.datastax.oss.driver.api.mapper.annotations.StatementAttributes;
+import model.Renter;
+
+@Dao
 public class RenterDao {
+
+    @StatementAttributes(consistencyLevel = "QUORUM")
+    @QueryProvider(providerClass = RenterProvider.class)
+    void create(Renter renter);
+
+    @StatementAttributes(consistencyLevel = "ONE", pageSize = 100)
+    @QueryProvider(providerClass = RenterProvider.class)
+    Renter findById(long personalID);
+
+    @StatementAttributes(consistencyLevel = "QUORUM")
+    @QueryProvider(providerClass = RenterProvider.class)
+    void update(Renter renter);
+
+    @StatementAttributes(consistencyLevel = "QUORUM")
+    @QueryProvider(providerClass = RenterProvider.class)
+    void remove(long personalID);
 }
--- src/main/java/dao/VolumeDao.java
+++ src/main/java/dao/VolumeDao.java
@@ -1,4 +1,30 @@
 package dao;
 
+
+import com.datastax.oss.driver.api.mapper.annotations.Dao;
+import com.datastax.oss.driver.api.mapper.annotations.QueryProvider;
+import com.datastax.oss.driver.api.mapper.annotations.StatementAttributes;
+import model.Book;
+import model.Publication;
+import model.Renter;
+import model.Volume;
+
+@Dao
 public class VolumeDao {
+
+    @StatementAttributes(consistencyLevel = "QUORUM")
+    @QueryProvider(providerClass = VolumeProvider.class, entityHelpers = {Book.class, Publication.class})
+    void create(Volume volume);
+
+    @StatementAttributes(consistencyLevel = "ONE", pageSize = 100)
+    @QueryProvider(providerClass = VolumeProvider.class, entityHelpers = {Book.class, Publication.class})
+    Volume findById(long vehicleId);
+
+    @StatementAttributes(consistencyLevel = "QUORUM")
+    @QueryProvider(providerClass = VolumeProvider.class, entityHelpers = {Book.class, Publication.class})
+    void update(Volume volume);
+
+    @StatementAttributes(consistencyLevel = "QUORUM")
+    @QueryProvider(providerClass = VolumeProvider.class, entityHelpers = {Book.class, Publication.class})
+    void remove(long vehicleId);
 }
