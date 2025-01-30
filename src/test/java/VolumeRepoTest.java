e07bab234251c6ce2678522f40874248be44bbce
247751mo <247751@edu.p.lodz.pl>
2025-01-30 18:00:54 +0100

    VolumeRepoTest

--- src/test/java/VolumeRepoTest.java
+++ src/test/java/VolumeRepoTest.java
@@ -1,8 +1,10 @@
 import model.Book;
 import model.Monthly;
 import model.Volume;
-import org.bson.Document;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import repositories.VolumeRepo;
 
 import java.util.List;
@@ -13,23 +15,25 @@ class VolumeRepoTest {
 
     private static VolumeRepo volumeRepo;
 
-
-    @BeforeEach
-    public void setUp() {
+    @BeforeAll
+    public static void setUp() {
         volumeRepo = new VolumeRepo();
-        volumeRepo.initDbConnection();
     }
 
-    @AfterEach
-    public void tearDown() {
-        volumeRepo.getDatabase().getCollection("volumes", Volume.class).deleteMany(new Document());
+    @BeforeEach
+    public void cleanUp() {
+        volumeRepo.getDatabase().getCollection("volumes", Volume.class).drop();
+    }
+
+    @AfterAll
+    public static void tearDown() {
+        volumeRepo.getDatabase().getCollection("volumes", Volume.class).drop();
         volumeRepo.close();
     }
 
-
     @Test
     void testAddVolume() {
-        Book book1 = new Book(1, "Solaris", "Scifi", "Stanislaw Lem");
+        Book book1 = new Book(1,"Solaris", "Scifi", "Stanislaw Lem");
         volumeRepo.create(book1);
         Book foundVolume = (Book) volumeRepo.read(1);
         assertEquals(book1.getTitle(), foundVolume.getTitle());
@@ -41,4 +45,56 @@ class VolumeRepoTest {
     }
 
 
+
+    @Test
+    void testRemoveVolume() {
+        Volume book1 = new Book(1,"Solaris","Scifi","Stanislaw Lem");
+        volumeRepo.create(book1);
+
+        Volume foundVolume = volumeRepo.read(book1.getVolumeId());
+        assertNotNull(foundVolume);
+
+        volumeRepo.delete(book1);
+
+        Volume deletedVolume = volumeRepo.read(book1.getVolumeId());
+        assertNull(deletedVolume);
+    }
+
+    @Test
+    void testUpdateVolume() {
+        Volume book1 = new Book(1,"Solaris","Scifi","Stanislaw Lem");
+        volumeRepo.create(book1);
+
+        Volume foundVolumeBeforeUpdate = volumeRepo.read(book1.getVolumeId());
+        assertEquals("Solaris", foundVolumeBeforeUpdate.getTitle());
+        assertEquals("Scifi", foundVolumeBeforeUpdate.getGenre());
+
+        book1.setTitle("NowyTytul");
+        book1.setGenre("ificS");
+        book1.setIsRented(1);
+
+        volumeRepo.update(book1);
+
+        Volume foundVolumeAfterUpdate = volumeRepo.read(book1.getVolumeId());
+        assertEquals("NowyTytul", foundVolumeAfterUpdate.getTitle());
+        assertEquals("ificS", foundVolumeAfterUpdate.getGenre());
+
+    }
+
+    @Test
+    void testGetAllVolumes() {
+        List<Volume> volumes = volumeRepo.readAll();
+        int initialSize = volumes.size();
+
+        Volume book1 = new Book(1,"Solaris","Scifi","Stanislaw Lem");
+        Volume monthly1 = new Monthly(2, "Miesiecznik","Gatunek","Wydawca");
+
+        volumeRepo.create(book1);
+        volumeRepo.create(monthly1);
+
+        volumes = volumeRepo.readAll();
+        int finalSize = volumes.size();
+
+        assertEquals(initialSize + 2, finalSize);
+    }
 }
